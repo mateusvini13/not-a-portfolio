e32a1770160f7e8007ce1ef542e874641e9b6739
Mateus Vinícius <mateus.oliveira21@fatecitapetininga.edu.br>
2019-06-16 23:27:59 -0300

    Uh, I dunno, there's a cube, I guess?

---
+++ components/home/index.js
@@ -0,0 +1,69 @@
+//Creates Scene
+var HEIGHT, WIDTH;
+var scene, camera, renderer;
+var cube, geometry, material;
+
+//Create Lights
+function createLights() {
+  light = new THREE.HemisphereLight(0xffffff, 0xffffff, .5)
+  
+  shadowLight = new THREE.DirectionalLight(0xffffff, .8);
+  shadowLight.position.set(200, 200, 200);
+  shadowLight.castShadow = true;
+  shadowLight.shadowDarkness = .2;
+ 	
+  backLight = new THREE.DirectionalLight(0xffffff, .4);
+  backLight.position.set(-100, 200, 50);
+  backLight.shadowDarkness = .1;
+  backLight.castShadow = true;
+ 	
+  scene.add(backLight);
+  scene.add(light);
+  scene.add(shadowLight);
+}
+
+//Resize Window
+function onWindowResize() {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+}
+
+//Renders screen
+function animate() {
+	requestAnimationFrame( animate );
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  
+	renderer.render( scene, camera );
+}
+
+function init(){
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+  renderer = new THREE.WebGLRenderer({alpha: true, antialias: true });
+
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer.setPixelRatio( window.devicePixelRatio );
+  renderer.shadowMapEnabled = true;
+
+  container = document.getElementById('world');
+  container.appendChild(renderer.domElement);
+
+  window.addEventListener('resize', onWindowResize, false);
+
+  //Adds Cube
+  geometry = new THREE.BoxGeometry( 1, 1, 1 );
+  material = new THREE.MeshLambertMaterial( { color: 0xeb56c3, shading: THREE.FlatShading } );
+
+  cube = new THREE.Mesh( geometry, material );
+  scene.add( cube );
+
+  createLights();
+  animate();
+
+  camera.position.z = 5
+}
+
+init()
