236a5b496261db862c0218b3fbe4020588c6cdea
Mateus Vinícius <mateus.oliveira21@fatecitapetininga.edu.br>
2019-06-18 00:34:27 -0300

    There's a Ghost now

--- components/home/index.js
+++ components/home/index.js
@@ -1,71 +1,120 @@
 //Creates Scene
-var HEIGHT, WIDTH;
-var scene, camera, renderer;
-var cube, geometry, material;
+var HEIGHT, WIDTH
+var scene, camera, renderer
+var cube, ghostScene, geometry, materials = []
+
+//Set canvas ratio when rendering
+var scaleX, scaleY
+
+//Creates Textures and Materials
+var textureLoader = new THREE.TextureLoader();
+
+//Suit
+var spoopyTexture = textureLoader.load('../../textures/spoopy/spoopy.png');
+spoopyTexture.flipY = false;
+spoopyTexture.anisotropy = 8;
+var spoopyMaterial = new THREE.MeshPhongMaterial( { map: spoopyTexture } )
+materials["Spoopy"] = spoopyMaterial;
+
+//hat
+var hatTexture = textureLoader.load('../../textures/spoopy/hat.png');
+hatTexture.flipY = false;
+hatTexture.anisotropy = 8;
+var hatMaterial = new THREE.MeshPhongMaterial( { map: hatTexture } )
+materials["Hat"] = hatMaterial;
+
+//Bow
+var bowMaterial = new THREE.MeshPhongMaterial( { color: 0xf22e48 } )
+materials["Bowtie"] = bowMaterial;
 
 //Create Lights
 function createLights() {
   light = new THREE.HemisphereLight(0xffffff, 0xffffff, .5)
   
-  shadowLight = new THREE.DirectionalLight(0xffffff, .8);
-  shadowLight.position.set(200, 200, 200);
-  shadowLight.castShadow = true;
-  shadowLight.shadowDarkness = .2;
+  shadowLight = new THREE.DirectionalLight(0xffffff, .8)
+  shadowLight.position.set(200, 200, 200)
+  shadowLight.castShadow = true
  	
-  backLight = new THREE.DirectionalLight(0xffffff, .4);
-  backLight.position.set(-100, 200, 50);
-  backLight.shadowDarkness = .1;
-  backLight.castShadow = true;
+  backLight = new THREE.DirectionalLight(0xffffff, .4)
+  backLight.position.set(-100, 200, 50)
+  backLight.castShadow = true
  	
-  scene.add(backLight);
-  scene.add(light);
-  scene.add(shadowLight);
+  scene.add(backLight)
+  scene.add(light)
+  scene.add(shadowLight)
 }
 
 //Resize Window
 function onWindowResize() {
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
+  var newWidth = ogWidth/window.innerWidth
+  var newHeigth = ogHeight/window.innerHeight
+  ghostScene.scale.set(newWidth, newHeigth, newWidth)
+
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.updateProjectionMatrix()
+  renderer.setSize(window.innerWidth, window.innerHeight)
 }
 
 //Renders screen
 function animate() {
-	requestAnimationFrame( animate );
+	requestAnimationFrame( animate )
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-  
-	renderer.render( scene, camera );
+  ghostScene.rotation.y += 0.01
+	renderer.render(scene, camera)
+}
+
+//Load Ghost
+function loadGhost(){
+  var loader = new THREE.GLTFLoader()
+
+  loader.load('../../models/spoopy.glb', function ( ghost ) {
+    ghostScene = ghost.scene
+
+    ghostScene.traverse((o) => {
+      if (o.isMesh) {
+        o.material = materials[o.name];
+      }
+    });
+
+    ogWidth = window.innerWidth * 75
+    ogHeight = window.innerHeight * 75
+    ghostScene.scale.set(75, 75, 75)
+
+    scene.add(ghostScene)
+    ghostScene.position.y = 60;
+
+    animate()
+  }, undefined, function (error) {
+    console.error(error)
+  })
 }
 
 function init(){
-  scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-  renderer = new THREE.WebGLRenderer({alpha: true, antialias: true });
+  scene = new THREE.Scene()
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+  
+  // camera = new THREE.PerspectiveCamera( 100, window.innerWidth / window.innerHeight, 0.1, 1000 )
+  camera = new THREE.OrthographicCamera( width / - 2, width / 2, height / 2, height / -2, -500, 500 )
+  renderer = new THREE.WebGLRenderer({alpha: true, antialias: true })
 
-  renderer.setSize( window.innerWidth, window.innerHeight );
-  renderer.setPixelRatio( window.devicePixelRatio );
-  renderer.shadowMapEnabled = true;
+  renderer.setSize( window.innerWidth, window.innerHeight )
+  renderer.setPixelRatio( window.devicePixelRatio )
+  renderer.shadowMap.Enabled = true
 
-  container = document.getElementById('world');
-  container.appendChild(renderer.domElement);
+  container = document.getElementById('world')
+  container.appendChild(renderer.domElement)
 
-  window.addEventListener('resize', onWindowResize, false);
+  window.addEventListener('resize', onWindowResize, false)
 
-  //Adds Cube
-  geometry = new THREE.BoxGeometry( 1, 1, 1 );
-  material = new THREE.MeshLambertMaterial( { color: 0xeb56c3, shading: THREE.FlatShading } );
-
-  cube = new THREE.Mesh( geometry, material )
-  cube.position.y = 0.5;
-
-  scene.add( cube );
-
-  createLights();
-  animate();
-
+  loadGhost()
+  createLights()
   camera.position.z = 5
 }
 
-init()
+if (WEBGL.isWebGLAvailable()) {
+	init()
+} else {
+	var warning = WEBGL.getWebGLErrorMessage()
+	document.getElementById('world').appendChild(warning)
+}
